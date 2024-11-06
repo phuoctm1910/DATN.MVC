07345bc695710938fe834e67981e1f6fba00c0d0
mike123691 <150338302+mike123691@users.noreply.github.com>
2024-11-06 18:06:34 +0700

    Feature/LoginMVC

---
+++ DATN.MVC/Request/Login/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DATN.MVC.Models.Request
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
---
+++ DATN.MVC/Respone/Login/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace DATN.MVC.Models.Response
+{
+    public class LoginResponse
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string RoleName { get; set; }
+        public string Token { get; set; }
+    }
+}
