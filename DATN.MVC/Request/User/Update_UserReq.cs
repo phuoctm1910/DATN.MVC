78b6da4eff3b55b0e2cc73d00921cf1f2b6def99
hieuphpc07025 <144969739+hieuphpc07025@users.noreply.github.com>
2024-11-06 19:09:20 +0700

    Code MVC chỉnh sửa thông tin

---
+++ DATN.MVC/Request/User/Update_UserReq.cs
@@ -0,0 +1,23 @@
+﻿namespace DATN.MVC.Request.User
+{
+    public class Update_UserReq
+    {
+
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string? Password { get; set; }
+        public string? FullName { get; set; }
+        public DateTime Birth { get; set; }
+        public bool Gender { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Image { get; set; } = "default.png";
+        public string? BackgroundImage { get; set; }
+        public int RoleId { get; set; }
+        public byte Status { get; set; }
+        public decimal Balance { get; set; }
+        public bool IsActived { get; set; } = true;
+        public int UpdatedDate { get; set; }
+    }
+}
