b66b1bc52ce133e1ee50429d8178fb2e0c83f3d1
miinhkha <miinhkha10@gmail.com>
2024-11-06 22:04:10 +0700

    mvc dang ky
    (cherry picked from commit 029ee26611714190c8c05b77c9208e31f5f0ef17)

---
+++ DATN.MVC/Request/Registration/RegistrationRequest.cs
@@ -0,0 +1,24 @@
+﻿using DATN.MVC.Helpers;
+using Newtonsoft.Json;
+
+namespace DATN.MVC.Request.Registration
+{
+    public class RegistrationRequest
+    {
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+
+        public DateTime Birth { get; set; }
+
+        public bool Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public long CreatedDateUnix { get; set; }
+
+        public string BirthFormatted
+        {
+            get => Birth.ToString("yyyy-MM-dd");
+        }
+    }
+}
