e449d9c03f966aafbbfce31f45693281ad344394
hieuphpc07025 <144969739+hieuphpc07025@users.noreply.github.com>
2024-11-06 19:09:20 +0700

    Code MVC chỉnh sửa thông tin
    (cherry picked from commit 78b6da4eff3b55b0e2cc73d00921cf1f2b6def99)

--- DATN.MVC/Controllers/UserController.cs
+++ DATN.MVC/Controllers/UserController.cs
@@ -1,6 +1,10 @@
 ﻿using DATN.MVC.Models;
+using DATN.MVC.Request.User;
+using DATN.MVC.Respone.User;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace DATN.MVC.Controllers
 {
@@ -112,6 +116,70 @@ namespace DATN.MVC.Controllers
             {
                 return View();
             }
+        }
+
+        private readonly string _apiBaseUrl = "https://localhost:7296/api/User/update";
+
+        public async Task<IActionResult> UpdateInformation()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                int id = 1;
+                HttpResponseMessage response = await httpClient.GetAsync($"https://localhost:7296/api/User/get/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    var userInfo = JsonConvert.DeserializeObject<User_GetUserInfoRes>(jsonData);
+                    return View(userInfo);
+                }
+                else
+                {
+                    ViewBag.Error = "Không thể tải thông tin người dùng. Vui lòng thử lại sau.";
+                    return View();
+                }
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateInformation(Update_UserReq request)
+        {
+            var file = Request.Form.Files["ImageFile"];
+            if (file != null && file.Length > 0)
+            {
+                var filePath = Path.Combine("wwwroot/images", file.FileName);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                request.Image = "/images/" + file.FileName;
+            }
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Error = "Không nhập đủ thông tin. Vui lòng thử lại.";
+                return View();
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                string jsonData = JsonConvert.SerializeObject(request);
+                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await httpClient.PutAsync(_apiBaseUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    ViewBag.Message = "Cập nhật thông tin thành công.";
+                    return Redirect("/");
+                }
+                else
+                {
+                    ViewBag.Error = "Đã xảy ra lỗi khi cập nhật thông tin. Vui lòng thử lại.";
+                }
+            }
+
+            return View();
         }
     }
 }
