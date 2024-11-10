0f65bdcb183566673e37153a0552bd8020528e5a
mike123691 <150338302+mike123691@users.noreply.github.com>
2024-11-11 03:33:16 +0700

    Feature MVC/ManageParentCategoty/MaiDuy
    (cherry picked from commit a1cf2ed431f22fadfe0f44b40012f4abef9714ef)

---
+++ DATN.MVC/Areas/Admin/Models/AdminCategory/Response/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace DATN.MVC.Areas.Admin.Models.AdminCategory.Response
+{
+    public class ApiResponse<T>
+    {
+        public int StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public List<T> Data { get; set; }
+    }
+}
