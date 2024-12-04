d077fb1577a323e76f1f9bc1e6c1bfe6a78ccf21
hieuphpc07025 <144969739+hieuphpc07025@users.noreply.github.com>
2024-12-04 15:41:03 +0700

    Update4_12

--- DATN.MVC/Controllers/UserController.cs
+++ DATN.MVC/Controllers/UserController.cs
@@ -40,6 +40,30 @@ namespace DATN.MVC.Controllers
             return View();
         }
 
+        public IActionResult SuggestFriends()
+        {
+            var viewSettings = new ViewSettings
+            {
+                ShowSidebar = false, // Tắt sidebar
+                ShowHeader = true,   // Bật header
+                ShowFriendList = false // Tắt danh sách bạn bè
+            };
+            ViewBag.ViewSettings = viewSettings;
+            return View();
+        }
+
+        public IActionResult RequestFriends()
+        {
+            var viewSettings = new ViewSettings
+            {
+                ShowSidebar = false, // Tắt sidebar
+                ShowHeader = true,   // Bật header
+                ShowFriendList = false // Tắt danh sách bạn bè
+            };
+            ViewBag.ViewSettings = viewSettings;
+            return View();
+        }
+
         public ActionResult Friends()
         {
             using (var httpClient = new HttpClient())
